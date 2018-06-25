7311324455bab8791b8a88cdaeb8bb44230c4255
Sebastián Gurin <sebastigurin@gmail.com>
2018-06-25 01:27:21 -0300

    candombed

--- src/candombed/ui/layout.tsx
+++ src/candombed/ui/layout.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import navbar from './navbar';
+import SplitPane from 'react-split-pane'
 
-export default () =>
-  (<div className="container-fluid">
-    {navbar}
-    hello
-</div>)
+export default () => {
+
+  return (<div className="container-fluid">
+    {navbar()}
+
+    <SplitPane
+      split="vertical"
+      minSize={50}
+      maxSize={300}
+      defaultSize={100}
+      className="primary"
+      pane1Style={{ background: '#eee' }}
+      resizerStyle={{ background: '#000' }}
+    >
+      <div />
+      <SplitPane split="horizontal" paneStyle={{ padding: '2em', fontStyle: 'italic' }} pane2Style={{ background: '#aaa4ba' }}>
+        <div>Hello...</div>
+        <div> ...world.</div>
+      </SplitPane>
+    </SplitPane>
+
+
+  </div>)
+}
